938595c4d2580409232bd19ece13d6b96663f600
mate user <mate@11367.hosted.kvmka.ru>
2022-04-19 17:56:56 +0300

    Lesson 4. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -22,14 +22,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mainapp',
-    'authnapp',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "mainapp",
+    "authnapp",
 ]
 
 MIDDLEWARE = [
@@ -128,4 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Auth model
-AUTH_USER_MODEL = 'authnapp.ShopUser'
+AUTH_USER_MODEL = "authnapp.ShopUser"
