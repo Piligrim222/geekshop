f0683608a83c4c20cc2059a53ebb9e245304b578
mate user <mate@11367.hosted.kvmka.ru>
2022-04-19 15:34:53 +0300

    Lesson 4. User login and logout

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,20 @@
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
 
 import mainapp.views as mainapp
 
-app_name = "mainapp"
-
 urlpatterns = [
-    path("", mainapp.products, name="index"),
-    path("<int:pk>/", mainapp.products, name="category"),
+    path("admin/", admin.site.urls),
+    path("", mainapp.main, name="main"),
+    path("products/", include("mainapp.urls", namespace="products")),
+    path("contact/", mainapp.contact, name="contact"),
+    path("auth/", include("authnapp.urls", namespace="auth")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
